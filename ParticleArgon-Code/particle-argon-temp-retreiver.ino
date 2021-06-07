0d1ed29e400b57fc131900c131a1a9e83a996bd9
Munatsi Matipano <40175171+Apoth3os1s@users.noreply.github.com>
2021-06-07 19:14:32 +1000

    Added Extra commenting

--- ParticleArgon-Code/particle-argon-temp-retreiver.ino
+++ ParticleArgon-Code/particle-argon-temp-retreiver.ino
@@ -9,7 +9,7 @@ DHT dht(DHTPIN, DHTTYPE);
 void setup() 
 {
 	dht.begin();
-	float temp;
+	float temp; // creates a variable of float for temp as the temp reading contains decimalas
 }
 //////////////////////////////////////////////////////
 void loop() 
@@ -18,12 +18,12 @@ void loop()
 	delay(500);
     float temp = 0;
 // Reads temperature as Celsius
-    while (temp < 5.0 || temp > 50){
+    while (temp < 5.0 || temp > 50){ // Checks to see if the temp reading is realistic if not it tries to collect again until it does
         temp = dht.getTempCelcius();
     }
 //Uploads the value to the webhook
-    if (temp > 5.0 && temp < 50){
-        Particle.publish("temp", String(temp), PRIVATE);
+    if (temp > 5.0 && temp < 50){ // Checks to see if the values that it has been passed are within a realistic range if not it does not send the value.
+        Particle.publish("temp", String(temp), PRIVATE);          // I added the second check as a fail safe as I somehow kept recieving 0 values 
         delay(1500);
     }
 }
